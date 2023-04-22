42e1ec19f83e074fbbd774f24f6b80982bb0c78f
Adrian Vinícius Roma Alves <adrianromaalves@hotmail.com>
2023-04-22 09:21:03 -0300

    fix: blank page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 8080,
+    watch: {
+      usePolling: true,
+    },
+  },
+  // "base" is used in production
+  base: "/rpg-menu/",
   plugins: [react()],
-})
+});
